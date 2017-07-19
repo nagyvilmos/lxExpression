533b1265f09492559869a80632ca5533ddaa101d
William Norman-Walker <william.norman.walker@gmail.com>
2017-07-19 08:48:04 +0100

    Add tests for inbuilt functions, use the lexa licence on all new files

---
+++ src/lxexpression/TestInternalFunction.java
@@ -0,0 +1,27 @@
+/*==============================================================================
+ * Lexa - Property of William Norman-Walker
+ *------------------------------------------------------------------------------
+ * TestInternalFunction.java (lxExpression)
+ *------------------------------------------------------------------------------
+ * Author:  William Norman-Walker
+ * Created: July 2017
+ *==============================================================================
+ */
+
+package lxexpression;
+
+import lexa.test.TestClass;
+
+/**
+ * Test all the internal functions
+ * @author  willaimnw
+ * @since   2017-07
+ */
+class TestInternalFunction extends TestClass
+{
+
+    public TestInternalFunction()
+    {
+    }
+
+}
